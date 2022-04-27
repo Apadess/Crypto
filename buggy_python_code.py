96afa2684be04d74b98bbe243c9e370b588b3c6b
unknown <xcepela@ad.fi.muni.cz>
2022-04-27 09:19:33 +0200

    nothing

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -6,6 +6,8 @@ import base64
 import subprocess
 import flask
 
+#hello
+
 # Input injection
 def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
